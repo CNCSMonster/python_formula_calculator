70d78ff6989b9d558d5fe8895deeb25869535133
lf666453 <106480663+lf666453@users.noreply.github.com>
2022-07-17 13:19:50 +0800

    Update cals.c

--- cncsmonster/c_impl/cals.c
+++ cncsmonster/c_impl/cals.c
@@ -1,20 +1,76 @@
-#include "cals.h"
+#åŸºæœ¬è¿ç®—æ¨¡å—
+def cal_once(num_stack, cal_stack):
+    # print("è®¡ç®—å‰num_stackä¸º:",num_stack)
+    # print("è®¡ç®—å‰cal_satckä¸º:",cal_stack)
+    num2 = num_stack.pop(-1)
+    num1 = num_stack.pop(-1)
+    cal_chr = cal_stack.pop(-1)
+    if cal_chr == "+":
+        return float(num1) + float(num2)
+    elif cal_chr == "-":
+        return float(num1) - float(num2)
+    elif cal_chr == "*":
+        return float(num1) * float(num2)
+    # elif cal_chr == 'tan':
+    #     return float(math.tan(num2*math.pi/180))
+    # elif cal_chr == 'sin':
+    #     return float(math.sin(num2*math.pi/180))
+    # elif cal_chr == 'cos':
+    #     return float(math.cos(num2*math.pi/180))
+    else:
+        return float(num1) / float(num2)
+
+def cal_stack_process(num_stack, cal_stack, current_cal):
+    if len(cal_stack) == 0:
+        cal_stack.append(current_cal)
+    else:
+        head_cal = cal_stack[-1]
+        head_level = level_dict[head_cal]
+        current_cal_level = level_dict[current_cal]
+        if current_cal_level <= head_level:
+            result = cal_once(num_stack, cal_stack)
+            num_stack.append(result)
+            cal_stack_process(num_stack, cal_stack, current_cal)
+        else:
+            cal_stack.append(current_cal)
 
 
-/*
-É¾³ı×Ö·û´®ÖĞµÄÎŞĞ§×Ö·û*/
-int mystrip(char* str,char* ig);
+def cal_with_no_brac(cal_str):
+    # print("å½“å‰è¦å¤„ç†çš„å­—ç¬¦ä¸²:",cal_str)
+    num_stack = []
+    cal_stack = []
+    i = 0
+    while i < len(cal_str):
+        temp_str = ""
+        while i < len(cal_str) and cal_str[i] not in ["(", ")", "+", "-", "*", "/",'sin','cos','tan']:
+            temp_str += cal_str[i]
+            i += 1
+        if len(temp_str): num_stack.append(temp_str)
+        if i < len(cal_str):
+            if cal_str[i] == "(":
+                left_index = i
+                while cal_str[i] != ")":
+                    i += 1
+                result = cal_with_no_brac(cal_str[left_index + 1:i])
+                num_stack.append(result)
+            else:
+                cal_stack_process(num_stack, cal_stack, cal_str[i])
+            i += 1
+
+    while len(cal_stack):
+        result = cal_once(num_stack, cal_stack)
+        num_stack.append(result)
+
+    return num_stack[0]
 
 
-//ÅĞ¶ÏÒ»¸ö×Ö·û´®ÊÇ·ñÊÇºÏ·¨µÄËÄÔòÔËËã¹«Ê½´®,Èç¹ûÊÇ·µ»Ø1£¬·ñÔò·µ»Ø0
-int if_sz(char* str){
+level_dict = {"*": 1, "/": 1, "+": 0, "-": 0}
+# cal_str="1+2.5/3+(6.7-9)"
+cal_str = input()
+print(cal_with_no_brac(cal_str))
 
-}
 
-//ÅĞ¶ÏÒ»¸ö×Ö·û´®ÊÇ·ñÊÇ¼òµ¥µÄÖ»ÓĞÒ»¸öÔËËã·ûµÄËÄÔòÔËËãÊ½×Ó
-int if_easy_sz(char* str){
 
-}
 
 
 
